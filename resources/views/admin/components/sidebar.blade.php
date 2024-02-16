ad2e9e62bf5216d604e1f3f79d2fb046db211e7e
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-16 17:56:15 +0600

    Component Setup

---
+++ resources/views/admin/components/sidebar.blade.php
@@ -0,0 +1,66 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!-- User details -->
+        <div class="user-profile text-center mt-3">
+            <div class="">
+                <img src="{{ asset('backend/images/users/avatar-1.jpg') }}" alt="" class="avatar-md rounded-circle">
+            </div>
+            <div class="mt-3">
+                <h4 class="font-size-16 mb-1">Julia Hudda</h4>
+                <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i> Online</span>
+            </div>
+        </div>
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title">Menu</li>
+
+                <li>
+                    <a href="index.html" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">3</span>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="calendar.html" class=" waves-effect">
+                        <i class="ri-calendar-2-line"></i>
+                        <span>Calendar</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="ri-mail-send-line"></i>
+                        <span>Email</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="email-inbox.html">Inbox</a></li>
+                        <li><a href="email-read.html">Read Email</a></li>
+                    </ul>
+                </li>
+
+                <li class="menu-title">Pages</li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="ri-account-circle-line"></i>
+                        <span>Authentication</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="auth-login.html">Login</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
+
