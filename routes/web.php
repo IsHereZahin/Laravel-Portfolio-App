68e556cd9769bee2ef5ff78444aee88cc72a675f
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-16 18:10:41 +0600

    login logout

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', function () {
-    return view('admin.index');
+
+Route::middleware('auth', 'verified')->group(function() {
+    Route::get('/dashboard', function() {
+        return view('admin.index');
+});
 });
 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
