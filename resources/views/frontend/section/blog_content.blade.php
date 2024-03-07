b22ca48d3eb8d3eeabd072e4452b55707039f2a0
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-03-07 11:53:59 +0600

    blog url update

--- resources/views/frontend/section/blog_content.blade.php
+++ resources/views/frontend/section/blog_content.blade.php
@@ -26,45 +26,45 @@
                 <div class="col-lg-4 col-md-6 col-sm-9">
                     <div class="blog__post__item">
                         <div class="blog__post__thumb">
-                            <a href="blog-details.html"><img src="{{ asset('frontend/assets/img/blog/blog_post_thumb02.jpg') }}" alt=""></a>
+                            <a href="#"><img src="{{ asset('frontend/assets/img/blog/blog_post_thumb02.jpg') }}" alt=""></a>
                             <div class="blog__post__tags">
                                 <a href="blog.html">Social</a>
                             </div>
                         </div>
                         <div class="blog__post__content">
                             <span class="date">13 january 2021</span>
-                            <h3 class="title"><a href="blog-details.html">Make communication Fast and Effectively.</a></h3>
-                            <a href="blog-details.html" class="read__more">Read mORe</a>
+                            <h3 class="title"><a href="#">Make communication Fast and Effectively.</a></h3>
+                            <a href="#" class="read__more">Read mORe</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-9">
                     <div class="blog__post__item">
                         <div class="blog__post__thumb">
-                            <a href="blog-details.html"><img src="{{ asset('frontend/assets/img/blog/blog_post_thumb02.jpg') }}" alt=""></a>
+                            <a href="#"><img src="{{ asset('frontend/assets/img/blog/blog_post_thumb02.jpg') }}" alt=""></a>
                             <div class="blog__post__tags">
                                 <a href="blog.html">Social</a>
                             </div>
                         </div>
                         <div class="blog__post__content">
                             <span class="date">13 january 2021</span>
-                            <h3 class="title"><a href="blog-details.html">Make communication Fast and Effectively.</a></h3>
-                            <a href="blog-details.html" class="read__more">Read mORe</a>
+                            <h3 class="title"><a href="#">Make communication Fast and Effectively.</a></h3>
+                            <a href="#" class="read__more">Read mORe</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-9">
                     <div class="blog__post__item">
                         <div class="blog__post__thumb">
-                            <a href="blog-details.html"><img src="{{ asset('frontend/assets/img/blog/blog_post_thumb03.jpg') }}" alt=""></a>
+                            <a href="#"><img src="{{ asset('frontend/assets/img/blog/blog_post_thumb03.jpg') }}" alt=""></a>
                             <div class="blog__post__tags">
                                 <a href="blog.html">Work</a>
                             </div>
                         </div>
                         <div class="blog__post__content">
                             <span class="date">13 january 2021</span>
-                            <h3 class="title"><a href="blog-details.html">How to increase your productivity at work - 2021</a></h3>
-                            <a href="blog-details.html" class="read__more">Read mORe</a>
+                            <h3 class="title"><a href="#">How to increase your productivity at work - 2021</a></h3>
+                            <a href="#" class="read__more">Read mORe</a>
                         </div>
                     </div>
                 </div>
